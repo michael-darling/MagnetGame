66a3e8a70ceb5ce8f44ad2246d1a6076c8cfa8fe
Michael Darling <michaeldarling683@gmail.com>
2024-08-04 01:27:47 -0500

    Make score text increment up 1 by 1

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
-using TMPro;
+using TMPro; // Import the TextMeshPro namespace
+using System.Collections;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance { get; private set; }
 
-    public TextMeshProUGUI scoreText; // Reference to the UI Text component that displays the score
+    public TextMeshProUGUI scoreText; // Reference to the TextMeshProUGUI component that displays the score
     private int score;
+    private int displayedScore;
+
+    public float scoreUpdateSpeed = 0.05f; // Speed of the score update in seconds
 
     void Awake()
     {
@@ -25,20 +29,31 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         score = 0;
+        displayedScore = 0;
         UpdateScoreText();
     }
 
     public void AddPoints(int points)
     {
         score += points;
-        UpdateScoreText();
+        StartCoroutine(UpdateScoreGradually());
+    }
+
+    private IEnumerator UpdateScoreGradually()
+    {
+        while (displayedScore < score)
+        {
+            displayedScore++;
+            UpdateScoreText();
+            yield return new WaitForSeconds(scoreUpdateSpeed);
+        }
     }
 
     private void UpdateScoreText()
     {
         if (scoreText != null)
         {
-            scoreText.text = score.ToString();
+            scoreText.text = displayedScore.ToString();
         }
     }
 }
