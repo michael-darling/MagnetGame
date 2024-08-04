282add05936d1a78879fafc89aa8a09ec5a42d0f
Michael Darling <michaeldarling683@gmail.com>
2024-08-04 15:35:14 -0500

    Don't start game until player moves

--- Assets/Scripts/TutorialKeyPrompt.cs
+++ Assets/Scripts/TutorialKeyPrompt.cs
@@ -1,16 +1,21 @@
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TutorialKeyPrompt : MonoBehaviour
 {
     [SerializeField]
     private KeyCode[] keyCodes;
 
+    [SerializeField]
+    private UnityEvent keyPressed;
+
     private void Update()
     {
         if (keyCodes.Any(key => Input.GetKeyDown(key)))
         {
             gameObject.SetActive(false);
+            keyPressed.Invoke();
         }
     }
 }
